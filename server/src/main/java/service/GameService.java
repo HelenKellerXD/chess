f341fc00b199c8ba298f17fb2a1e842387294d28
Slade Sands <ssullivansands@gmail.com>
2025-06-30 11:41:37 -0600

    edited to pull request and result files from the shared file instead of server file

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -5,6 +5,10 @@ import dataaccess.GameDAO;
 import dataaccess.MemoryGameDAO;
 import dataaccess.MySQLGameDAO;
 import model.GameData;
+import request.CreateGameRequest;
+import request.JoinGameRequest;
+import result.CreateGameResult;
+import result.ListGamesResult;
 
 import java.util.Collection;
 
