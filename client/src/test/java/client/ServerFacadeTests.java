290bab8511bd5aa286b0b9d1b11251c8fcaf6c38
Slade Sands <ssullivansands@gmail.com>
2025-06-30 11:30:27 -0600

    added a client communicator function that conatains code that was originally in the Serverfacade file

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,18 +2,31 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import ui.ServerFacade;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        //facade = new ServerFacade(port);
+
     }
+
+    @BeforeEach
+    void resetServer() {
+        server.stop();
+        server.run(0);
+    }
+
 
     @AfterAll
     static void stopServer() {
@@ -23,7 +36,13 @@ public class ServerFacadeTests {
 
     @Test
     public void sampleTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
 
+
+    @Test
+    void register() throws Exception {
+        //var authData = facade.register("player1", "password", "p1@email.com");
+        //assertTrue(authData.authToken().length() > 10);
+    }
 }
