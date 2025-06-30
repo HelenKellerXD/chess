3928dfb040951a96784544cc3479a5adc2be1b77
Slade Sands <ssullivansands@gmail.com>
2025-06-30 11:41:04 -0600

    game client is created with minimal functionality currently added, will add more for the next phase at a later date

--- client/src/main/java/ui/GameClient.java
+++ client/src/main/java/ui/GameClient.java
@@ -1,4 +1,56 @@
 package ui;
 
+import java.util.Arrays;
+
 public class GameClient {
+    //make sure to have a leave function to leave the game
+    private final ServerFacade server;
+    private final String serverUrl;
+    private final Repl repl;
+    private State state = State.PRELOGIN;
+
+
+    /*** list all the possible PostLogin actions
+     * - help
+     * - logout
+     * - create game
+     * - List Games
+     * - Play Game
+     * - Observe Game
+     */
+    public GameClient(String serverUrl, Repl repl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+        this.repl = repl;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "help" -> help();
+                case "leave" -> "leave";
+                default -> help();
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+
+
+
+
+    public String help() {
+        return """
+                [Options] : [what to type]
+                - Go Back to menu: "leave"
+                - Help: "help"
+                
+                """;
+
+    }
+
 }
