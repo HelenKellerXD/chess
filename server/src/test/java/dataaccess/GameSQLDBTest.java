111b67b0f5f8658e36ac4d4669af89a4235ca6ad
Slade Sands <ssullivansands@gmail.com>
2025-07-22 10:58:54 -0600

    added a Game SQL test file

---
+++ server/src/test/java/dataaccess/GameSQLDBTest.java
@@ -0,0 +1,69 @@
+package dataaccess;
+
+import chess.ChessGame;
+import org.junit.jupiter.api.*;
+import passoff.model.*;
+import server.Server;
+
+import java.lang.reflect.Method;
+import java.sql.*;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+public class GameSQLDBTest {
+    GameDAO gameDAO;
+
+    @BeforeEach
+    void initiate(){
+        Assertions.assertDoesNotThrow(() ->
+        {
+            gameDAO = new MySQLGameDAO();
+        });
+    }
+
+    @BeforeEach
+    void wipe(){
+        Assertions.assertDoesNotThrow(() ->
+        {
+            gameDAO.clear();
+        });
+    }
+
+    @Test
+    @Order(1)
+    void createGamePass(){}
+    void createGameFail(){}
+
+
+    @Test
+    @Order(2)
+    void getGamePass(){}
+    void getGameFail(){}
+
+    @Test
+    @Order(3)
+    void listGamesPass(){}
+    void listGamesFail(){}
+
+    @Test
+    @Order(4)
+    void addCallersPass(){}
+    void addCallersFail(){}
+
+    @Test
+    @Order(5)
+    void clearGamePass(){
+        Assertions.assertDoesNotThrow(() ->
+        {
+            gameDAO.clear();
+        });
+        int dBSize = -1;
+        Assertions.assertDoesNotThrow(() -> {
+                    dBSize = gameDAO.listGames().size();
+                });
+        Assertions.assertEquals(0, dBSize);
+    }
+}
