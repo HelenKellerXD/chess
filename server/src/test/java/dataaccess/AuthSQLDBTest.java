38444975a306b9126df3adf9c4004576f2a3bbd5
Slade Sands <ssullivansands@gmail.com>
2025-07-23 10:47:02 -0600

    completed all auth tests

--- server/src/test/java/dataaccess/AuthSQLDBTest.java
+++ server/src/test/java/dataaccess/AuthSQLDBTest.java
@@ -1,15 +1,8 @@
 package dataaccess;
 
-import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
 import org.junit.jupiter.api.*;
-import passoff.model.*;
-import server.Server;
-
-import java.lang.reflect.Method;
-import java.sql.*;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Supplier;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
@@ -24,7 +17,7 @@ public class AuthSQLDBTest {
         });
     }
 
-    @BeforeEach
+    @AfterEach
     void wipe(){
         Assertions.assertDoesNotThrow(() ->
         {
@@ -34,8 +27,84 @@ public class AuthSQLDBTest {
 
     @Test
     @Order(1)
-    void createAuthPass(){}
-    void createAuthFail(){}
+    void createAuthPass(){
+        Assertions.assertDoesNotThrow(() ->
+        {
+            String token = authDAO.createAuth("john");
+
+            Assertions.assertNotEquals(null, token);
+        });
+    }
+    @Test
+    @Order(2)
+    void createAuthFail(){
+        Assertions.assertDoesNotThrow(() ->
+        {
+            String token = authDAO.createAuth("john");
+            AuthData authData = authDAO.getAuth(token);
+            Assertions.assertEquals(authDAO.getAuth(token).username(), "john");
+        });
+    }
+
+
+    @Test
+    @Order(3)
+    void getAuthPass(){
+        Assertions.assertDoesNotThrow(() ->
+        {
+            String token = authDAO.createAuth("john");
+            AuthData authData = authDAO.getAuth(token);
+            Assertions.assertEquals(authData.username(), "john");
+        });
+
+    }
+
+    @Test
+    @Order(4)
+    void getAuthFail(){
+        Assertions.assertDoesNotThrow(() ->
+        {
+            String token = authDAO.createAuth("john");
+            AuthData authData = authDAO.getAuth(token);
+            Assertions.assertNotEquals(authData.username(), "craig");
+        });
+    }
+
+    @Test
+    @Order(5)
+    void deleteAuthPass(){
+        Assertions.assertDoesNotThrow(() ->
+        {
+            String token = authDAO.createAuth("john");
+            authDAO.deleteAuth(token);
+            Assertions.assertEquals(authDAO.getAuth(token), null);
+        });
+
+
+    }
+
+    @Test
+    @Order(6)
+    void deleteAuthFail(){
+        Assertions.assertDoesNotThrow(() ->
+        {
+            String token = authDAO.createAuth("john");
+            AuthData authData = authDAO.getAuth(token);
+            authDAO.deleteAuth(token);
+            Assertions.assertNotEquals(authData.username(), authDAO.getAuth(token));
+        });
+    }
+
+
+
+    @Test
+    @Order(9)
+    void clearAuthPass(){
+        Assertions.assertDoesNotThrow(() ->
+        {
+            authDAO.clear();
+        });
+    }
 
 
 }
